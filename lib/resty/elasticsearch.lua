5b1bf694acb4277a3f857648cdd05f648bb76137
bells <bellszhu@gmail.com>
2016-07-06 19:44:05 +0800

    增加search_explain等

--- lib/resty/elasticsearch.lua
+++ lib/resty/elasticsearch.lua
@@ -10,6 +10,19 @@ local _M = {
 
 local mt = { __index = _M }
 
+local function _make_path(index, doc_type, ...) 
+    local prefix
+    if index ~= nil and doc_type ~= nil then
+        prefix = string.format('%s/%s', index, doc_type)
+    elseif index ~= nil and doc_type == nil then
+        prefix = index
+    elseif index == nil and doc_type ~= nil then
+        prefix = '_all/' .. doc_type
+    end
+    local suffix = table.concat({...}, '/')
+    return string.format('/%s/%s', prefix, suffix)
+end
+
 
 function _M.new(self, hosts)
 	if not hosts then
@@ -23,44 +36,49 @@ function _M.new(self, hosts)
         table.insert(http_clients, http_client)
     end
 
-	return setmetatable({http_clients_len = #http_clients, http_clients = http_clients}, mt)
+	return setmetatable(
+        {http_clients_len = #http_clients, http_clients = http_clients},
+        mt
+    )
 end
 
 
 function _M._perform_request(self, http_method, url, params, body)
 	local temp_index = math.random(self.http_clients_len)
 	local http_client = self.http_clients[temp_index]
-	
+    
+    ngx.log(ngx.ERR, '****************: url: ', url)	
 	if params then
         url = string.format('%s?%s', url, ngx.encode_args(params))
 	end
-	local res, err = http_client:request{path = url, method = http_method, body = body}
+    if body then
+        body = cjson.encode(body)
+    else
+        body = ''
+    end
+
+	local res, err = http_client:request{
+        path = url, method = http_method, body = body}
 	if not res then
 		return nil, err
 	end
-    local body = ''
+    local response_body = ''
     if res.has_body then
-       body = res:read_body() 
+       response_body = res:read_body() 
     end
-	return res.stauts, cjson.decode(body)
-end
 
-
-function _M._make_path(self, ...) 
-    local path = table.concat({...}, '/')
-    ngx.log(ngx.ERR, '...........path: ', path)
-    return '/' .. path
+	return res.stauts, cjson.decode(response_body)
 end
 
 
 function _M.info(self, params)
-	_, data = self:_perform_request('GET', '/', params, nil)
+	_, data = self:_perform_request('GET', '/', params)
 	return data
 end
 
 
 function _M.ping(self, params)
-	local res, err = self:_perform_request('HEAD', '/', params, nil)
+	local res, err = self:_perform_request('HEAD', '/', params)
 	if not res then
 		return false
     end
@@ -68,13 +86,23 @@ function _M.ping(self, params)
 end
 
 
-function _M.search(self, index, doc_type, params, body)
-    if doc_type and not index then
-        index = '_all'
-    end
-    local _, data = self:_perform_request('GET', self:_make_path(index, doc_type, '_search'), params, cjson.encode(body))
+function _M.search(self, s_params)
+    local _, data = self:_perform_request(
+        'GET', _make_path(s_params.index, s_params.doc_type, '_search'),
+        s_params.params, s_params.body
+    )
+
     return data
 end
 
+
+function _M.search_template(self, s_params)
+    local _, data = self:_perform_request(
+        'GET', _make_path(s_params.index, s_params.doc_type, '_search', 'template'),
+        s_params.params, s_params.body
+    )
+
+    return data
+end
 
 return _M
